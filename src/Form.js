640ed59e4340024e26f9b331aeabff7b3355abc8
Habibi Ayoub <habibiayoub87@gmail.com>
2020-11-10 21:04:38 +0100

    Templates

---
+++ src/Form.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Form extends Component {
+    render() {
+        return(
+            <>
+                Image ici
+                <h1>Commentaire</h1>
+                Formulaire
+            </>
+        )
+    }
+}
+
+export default Form;
